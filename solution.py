0a77452a5f8439c85de7a09b2df151a24e8012fc
PoojithaYadavalli <poojithareddyyadavalli@gmail.com>
2020-07-08 14:28:14 +0530

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,3 +1,17 @@
+class Node:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def binaryTree(arr, root, i, n):
+    if i < n: 
+        temp = Node(arr[i])  
+        root = temp 
+        root.left = insertLevelOrder(arr, root.left, 2 * i + 1, n) 
+        root.right = insertLevelOrder(arr, root.right, 2 * i + 2, n)
+    return root
+
 def largestValues(root):
         if not root:
             return None
@@ -19,3 +33,7 @@ def largestValues(root):
             levels = node_arr
         return " ".join(res)
  
+tree = input().split()
+root1=None
+root=binaryTree(tree,root1,0,len(tree))
+print(largestValues(root))
